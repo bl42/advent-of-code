79f50962a1ec5765cd66e003b39a7efd63afb806
Ben Lacy <benlacy5@gmail.com>
2021-12-10 03:29:50 +0000

    Day 7: The Treachery of Whales Completed

---
+++ 2021/src/day7.rs
@@ -0,0 +1,62 @@
+use std::fs;
+
+// https://adventofcode.com/2021
+// Day 7: The Treachery of Whales
+
+pub fn run() {
+    let input = fs::read_to_string("./inputs/7.txt").expect("File required");
+
+    println!("Day 7 Result - Part 2: {:?}", solution(&input));
+}
+
+fn solution(input: &str) -> i64 {
+    let mut crabs: Vec<i64> = input
+        .split(',')
+        .map(|v| v.trim().parse().unwrap())
+        .collect();
+
+    crabs.sort();
+
+    let mut positions = crabs.clone();
+    positions.dedup();
+
+    let mut lowest_cost = i64::MAX;
+    for position in positions[0]..positions[positions.len() - 1] {
+        let fuel = cost_of_fuel(&crabs, position);
+
+        if fuel > lowest_cost {
+            break;
+        } else {
+            lowest_cost = fuel
+        };
+    }
+
+    lowest_cost
+}
+
+fn cost_of_fuel(crabs: &Vec<i64>, position: i64) -> i64 {
+    let distance: Vec<i64> = crabs
+        .iter()
+        .map(|crab| {
+            let dist = (crab - position).abs();
+            match dist {
+                0 => 0,
+                1 => 1,
+                d => ((1..d + 1).reduce(|a, b| a + b)).unwrap(),
+            }
+        })
+        .collect();
+    distance.iter().sum::<i64>()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const INPUT: &str = "16,1,2,0,4,2,7,1,2,14";
+
+    #[test]
+    fn first() {
+        assert_eq!(solution(INPUT), 168);
+    }
+}
--- 2021/src/main.rs
+++ 2021/src/main.rs
@@ -7,6 +7,7 @@ mod day3;
 mod day4;
 mod day5;
 mod day6;
+mod day7;
 
 fn main() {
     // day1::run();
@@ -14,5 +15,6 @@ fn main() {
     // day3::run();
     // day4::run();
     // day5::run();
-    day6::run();
+    //day6::run();
+    day7::run();
 }
