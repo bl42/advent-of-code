7e4d61c873d65121562c58ade8dcce7298a997ae
Ben Lacy <benlacy5@gmail.com>
2022-12-05 23:42:16 -0600

    Day 6

---
+++ 2022/src/day6/mod.rs
@@ -0,0 +1,47 @@
+// https://adventofcode.com/2022/day/6
+// --- Day 6 ---
+use std::collections::HashSet;
+
+fn find_start(input: &str, marker: usize) -> usize {
+    let s = for (i, arr) in input.as_bytes().windows(marker).enumerate() {
+        let mut set = HashSet::new();
+
+        for item in arr {
+            set.insert(item);
+        }
+
+        if set.len() == marker {
+            return i + marker;
+        }
+    };
+    return 0;
+}
+
+fn part1(input: &str) -> usize {
+    return find_start(input, 4);
+}
+
+fn part2(input: &str) -> usize {
+    return find_start(input, 14);
+}
+
+pub fn run() {
+    let input = include_str!("input.txt");
+
+    println!("Day 6.1: {} \t\t Day 6.2: {}", part1(&input), part2(&input));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const SAMPLE_INPUT: &str = include_str!("input.sample.txt");
+
+    #[test]
+    fn first() {
+        assert_eq!(find_start("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4), 7);
+    }
+    #[test]
+    fn second() {
+        assert_eq!(find_start("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14), 19);
+    }
+}
--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -6,6 +6,7 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
 
 fn main() {
     day1::run();
@@ -13,4 +14,5 @@ fn main() {
     day3::run();
     day4::run();
     day5::run();
+    day6::run();
 }
