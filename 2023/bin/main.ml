219cf559bf5880431517c9c2091f6356fa4fff33
Ben Lacy <benlacy5@gmail.com>
2023-12-02 15:50:06 -0600

    Day one part 2

--- 2023/bin/main.ml
+++ 2023/bin/main.ml
@@ -6,16 +6,37 @@ let read_lines file =
     String.split_lines x)
 ;;
 
-let find_int line ~f:filterList =
-  let num =
-    String.to_list line |> List.filter ~f:Char.is_digit |> filterList |> Char.to_int
-  in
-  num - Char.to_int '0' |> Int.to_string
+let filter_decode_number list =
+  match list with
+  | 'o' :: 'n' :: 'e' :: _ -> Some 1
+  | 't' :: 'w' :: 'o' :: _ -> Some 2
+  | 't' :: 'h' :: 'r' :: 'e' :: 'e' :: _ -> Some 3
+  | 'f' :: 'o' :: 'u' :: 'r' :: _ -> Some 4
+  | 'f' :: 'i' :: 'v' :: 'e' :: _ -> Some 5
+  | 's' :: 'i' :: 'x' :: _ -> Some 6
+  | 's' :: 'e' :: 'v' :: 'e' :: 'n' :: _ -> Some 7
+  | 'e' :: 'i' :: 'g' :: 'h' :: 't' :: _ -> Some 8
+  | 'n' :: 'i' :: 'n' :: 'e' :: _ -> Some 9
+  | ch :: _ when Char.is_digit ch -> Some (Char.to_int ch - Char.to_int '0')
+  | _ -> None
 ;;
 
+let prepare_calibration str =
+  let rec aux list str_list =
+    match filter_decode_number str_list, List.tl str_list with
+    | Some x, None -> x :: list
+    | None, None -> list
+    | Some x, Some tl -> aux (x :: list) tl
+    | None, Some tl -> if List.is_empty tl then list else aux list tl
+  in
+  aux [] (String.to_list str)
+;;
+
+let find_int line ~f:filterList = prepare_calibration line |> filterList |> Int.to_string
+
 let calibration acc str =
-  let first = find_int ~f:List.hd_exn str in
-  let last = find_int ~f:List.last_exn str in
+  let first = find_int ~f:List.last_exn str in
+  let last = find_int ~f:List.hd_exn str in
   acc + (Int.of_string @@ first ^ last)
 ;;
 
